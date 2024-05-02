9ae0e583670c87db8a42d3b9029abef981651b5d
mktdigbp <118197705+bpwebmkt@users.noreply.github.com>
2024-05-02 16:31:05 -0300

    Initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  // live bp seguradora sign up
+  // 1,2,3,4,5,6,7,8,9
+
+  // start:
+  // end:
+
+  return (
+    <>
+      <div>s</div>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Counter.tsx
@@ -0,0 +1,28 @@
+const Counter = () => {
+  const timer = [
+    {
+      id: 1,
+      name: "1a",
+    },
+    {
+      id: 2,
+      name: "2b",
+    },
+    {
+      id: 3,
+      name: "3c",
+    },
+    {
+      id: 4,
+      name: "4d",
+    },
+  ];
+
+  return (
+    <div className="border">
+      <h1>Counter</h1>
+    </div>
+  );
+};
+
+export default Counter;
