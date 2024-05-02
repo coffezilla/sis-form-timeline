fec322d0a14f218704adcfd4dfe6f048ba8c6003
mktdigbp <118197705+bpwebmkt@users.noreply.github.com>
2024-05-02 17:33:15 -0300

    added events bp seguradora

--- my-app/src/components/Counter.tsx
+++ my-app/src/components/Counter.tsx
@@ -1,5 +1,5 @@
 const Counter = () => {
-  const timer = [
+  const events = [
     {
       id: 1,
       name: "1a",
@@ -10,13 +10,63 @@ const Counter = () => {
     },
     {
       id: 3,
-      name: "3c",
+      name: "1a",
     },
     {
       id: 4,
-      name: "4d",
+      name: "2b",
+    },
+    {
+      id: 5,
+      name: "1a",
+    },
+    {
+      id: 1,
+      name: "1a",
+    },
+    {
+      id: 2,
+      name: "2b",
+    },
+    {
+      id: 3,
+      name: "1a",
+    },
+    {
+      id: 4,
+      name: "2b",
+    },
+    {
+      id: 5,
+      name: "1a",
     },
   ];
+
+  const fullTimer = 30;
+  const totalEvents =
+    events.length <= fullTimer / 2 ? events.length : fullTimer / 2;
+
+  // create tray
+  const gapBetweenEvents = fullTimer / totalEvents;
+
+  let currentEvent = 0;
+  let count = 0;
+  for (let i = 0; i < fullTimer; i++) {
+    count += 1;
+
+    if (count === gapBetweenEvents) {
+      count = 0;
+      const hasEventsLeft = currentEvent < totalEvents;
+      if (hasEventsLeft) {
+        currentEvent += 1;
+        console.log("event:", currentEvent);
+      } else {
+        console.log("has no event");
+      }
+    } else {
+      console.log("pass", i);
+    }
+  }
 
   return (
     <div className="border">
